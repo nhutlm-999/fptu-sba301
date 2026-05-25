eab6e49ef09afb65f0f999e017593358e56fb218
chaodau <nhutlm@chaodaus-MacBook-Pro.local>
2026-05-25 11:37:58 +0700

    Slot 9: Create Authorization Server OAuth2

---
+++ auth-server/src/main/java/vn/edu/fpt/authserver/config/AppSecurityConfig.java
@@ -0,0 +1,48 @@
+package vn.edu.fpt.authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig {
+    @Bean
+    SecurityFilterChain authorizationServerFilterChain(HttpSecurity http) throws Exception {
+        // Open default endpoints for authentication and registration, secure all other endpoints
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults()); // Enable OpenID Connect 1.0
+        return http.formLogin(Customizer.withDefaults()).build();
+
+    }
+
+     @Bean
+     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+         http
+                 .authorizeHttpRequests(auth -> auth
+                         .anyRequest().authenticated())
+                 .formLogin(Customizer.withDefaults());
+         return http.build();
+     }
+
+     @Bean
+     UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+     }
+}
